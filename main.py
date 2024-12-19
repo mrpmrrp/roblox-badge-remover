b36a0c3bfdd528747dc0e28c168e371f819b367e
sleeper <81833822+mrpmrrp@users.noreply.github.com>
2024-12-19 03:09:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,67 @@
-import requests, asyncio
+import json, requests, os
 
-Cookie = input('Enter your ROBLOX account cookie:   ')
+from platform import system
+from colorama import Fore, init
+from typing import List
 
-if (Cookie.startswith('.ROBLOSECURITY=') != True):
-    Cookie = f'.ROBLOSECURITY={Cookie}'
+init()
 
-global user_data
-with requests.get('https://www.roblox.com/mobileapi/userinfo', headers = { 'Cookie': Cookie}) as validation_response:
-    try:
-        user_data = validation_response.json()
-        
-        global user_id
+with open('config.json', 'r') as file:
+    config= file.read()
+    config= json.loads(config)
 
-        # user_id  = response_json['UserId']
-        username = user_data['UserName']
-        user_id = user_data['UserID']
+    file.close()
+    del file
 
-        print(f'Logged in as: {username} ({user_id})') 
-    except:
-        print('fatal error')
-        exit()
-
-async def get_token():
-    response = requests.post('https://auth.roblox.com/v2/logout', headers = { 'Cookie': Cookie })
-    return response.headers.get('x-csrf-token')
-
-async def get_badges(user_id, token):
-    response = requests.get(f'https://badges.roblox.com/v1/users/{user_id}/badges', headers = {
-        'Cookie': Cookie,
-        'x-csrf-token': token,
-        'content-type': 'application/json'
-    })
-
-    return response.json()
-
-async def remove_badge(badge_id, token):
-    response = requests.delete(f'https://badges.roblox.com/v1/user/badges/{badge_id}', headers = {
-        'Cookie': Cookie,
-        'x-csrf-token': token
-    })
-
-    print(f'badge id: {badge_id} - response code {response}')
-
-async def main():
-    print('Starting.. please wait.')
+def fetch_badges(UserId: int) -> List[str]:
+    Badges = []
+    next_page_cursor = ''
 
     while True:
-        Token = await get_token()
+        response: requests.Response = requests.get(f'https://badges.roblox.com/v1/users/{UserId}/badges?limit=10&cursor={next_page_cursor}&sortOrder=Asc')
+        response_json: json = response.json()
 
-        AllBadges = await get_badges(user_data['UserID'], Token)
-        Badges = AllBadges['data']
-        
-        if len(Badges) < 1:
+        [Badges.append(cell.get('id')) for cell in response_json.get('data')]
+        next_page_cursor = response_json.get('nextPageCursor')
+        if next_page_cursor is None:
             break
-
-        for Badge in Badges:
-            await remove_badge(Badge['id'], Token)
     
-    print('Done! exiting.')
-    exit()
+    return Badges
+
+def fetch_csrf(Cookie: str) -> str:
+    return requests.post('https://auth.roblox.com/v2/logout', cookies= {'.ROBLOSECURITY': Cookie}).headers.get('x-csrf-token')
+
+def delete_badge(Cookie: str, BadgeId: int) -> None:
+    csrf: requests.Response = fetch_csrf(Cookie)
+
+    try:
+        response: requests.Response = requests.delete(f'https://badges.roblox.com/v1/user/badges/{BadgeId}', headers= { 'X-CSRF-TOKEN': csrf }, cookies={".ROBLOSECURITY": Cookie})
+        if (response.status_code != 200):
+            raise Exception(response.json().get('errors')[0]['message'])
+        print(Fore.GREEN + f'Deleted {BadgeId}')
+    except Exception as error_message: 
+        print(f'{error_message}')
+    finally:
+        response.close()
+
+
+def main(Cookie: str) -> None:
+    user_data = requests.get('https://users.roblox.com/v1/users/authenticated', cookies={ ".ROBLOSECURITY": Cookie })
+    UserId: int = user_data.json().get('id')   
+    Username: str = user_data.json().get('name')
+
+    print(f'Logged in as {Username}')
+    
+    Badges: List[str]= fetch_badges(UserId)
+    [delete_badge(Cookie=Cookie, BadgeId=Badge) for Badge in Badges]
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    os.system('cls' if (system().lower() == 'windows') else 'clear')
+    
+    Cookie= config.get('Cookie')
+    if len(Cookie) < 1:
+        print('Invalid cookie')
+        exit()
+
+    main(Cookie)
